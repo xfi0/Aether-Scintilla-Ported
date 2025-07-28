fbbdc81ae794e5676e65636f7ca5240bdc37ff32
dom <166765848+xfi0@users.noreply.github.com>
2025-07-28 04:33:57 -0500

    Add project files.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(StupidTemplate.Menu.Main), "AetherTempRexonPorted", "1.0.0", "iidk && xfi0")]
+[assembly: MelonGame()]
---
+++ Patches/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace StupidTemplate.Patches
+{
+    public class HarmonyPatches
+    {
+        public static void OnEnable()
+        {
+            Menu.ApplyHarmonyPatches();
+        }
+
+        private void OnDisable()
+        {
+            Menu.RemoveHarmonyPatches();
+        }
+    }
+}
